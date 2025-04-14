9c52614bf98391cfdfdf6c2becd90b44acc7c59f
lemming <lemmingway@mail.ru>
2025-04-14 23:28:57 +0500

    hintbox fixed

--- src/components/hintbox/index.ts
+++ src/components/hintbox/index.ts
@@ -4,8 +4,15 @@ import './index.css';
 class ToggleButton extends DOMComponent {
 	constructor(domNode: Element) {
 		super(domNode);
-		domNode.addEventListener("click", () => {
-			domNode.classList.toggle("active");
+		domNode.addEventListener("click", event => {
+			if (!domNode.classList.contains('active')) {
+				setTimeout( () => domNode.classList.add("active"), 0 );
+			}
+		});
+		document.addEventListener("click", event => {
+			if (domNode.classList.contains('active') && !( event.target as HTMLElement ).closest('.hint-box')) {
+				domNode.classList.remove("active");
+			}
 		});
 	}
 }
